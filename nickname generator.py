e50081198160094522f5f64e68ab88232b15f407
KuttaBanda <dreamzempire7@gmail.com>
2022-09-07 12:51:00 -0600

    Update nickname generator.py

---
+++ nickname generator.py
@@ -0,0 +1,23 @@
+import pip
+
+
+import random
+import numpy as np
+
+loop = True
+while loop:
+    print("")
+    print("Main Menu")
+    print("1. Change Name")
+    print("2. Display a Random Nickname")
+    print("3. Display All Nicknames")
+    print("4. Add a Nickname")
+    print("5. Remove a Nickname")
+    print("6. Exit")
+    selection = input("What Option? (1-6): ")
+
+    if selection=="1":
+        print("CHANGE NAME")
+        first_name=input("Please enter first name: ")
+        last_name=input("Please enter last name: ")
+        print("Current name is " + first_name , " " + last_name + ".")
