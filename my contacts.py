87aa3c11f01344de20ed02d6e7ee9bdc174a9a70
KuttaBanda <dreamzempire7@gmail.com>
2022-09-12 13:22:58 -0600

    Update my contacts.py

--- my contacts.py
+++ my contacts.py
@@ -7,7 +7,7 @@ import random
 
 names=["Kyrie", "LeBron", "Luka"]
 phone=["578-389-2891", "389-289-2783", "366-287-2378"]
-email=["bestdriibles@hotmail.com", "theking@yahoo.com", "sloveniandude@gmail.com"]
+email=["bestdribbles@hotmail.com", "theking@yahoo.com", "sloveniandude@gmail.com"]
 
 contacts=np.matrix([names, phone, email])
 
@@ -27,3 +27,30 @@ while loop:
         print("DISPLAY CONTACT NAMES")
         for elem in names:
             print(elem)
+    elif selection=="2":
+        print("SEARCH FOR CONTACT")
+        name_search=input("Who's contact name do you want to search?: ")
+        l1=[name_search]
+        for i in l1:
+            if i in names:
+                index_name=names.index(name_search)
+                print("")
+                print(name_search)
+                print(contacts[1, index_name])
+                print(contacts[2, index_name])
+            else:
+                print("Name doesn't exist in contact list.")
+    elif selection=="3":
+        print("EDIT CONTACT")
+        edit_name=input("Who's contact info do you want to change?: ")
+        l2=[edit_name]
+        for i in l2:
+            if i in names:
+                indexof_name=names.index(edit_name)
+                edited_num=input("What is the new number?: ")
+                contacts[1,indexof_name]=edited_num
+                edited_email=input("What is the new email?: ")
+                contacts[2, indexof_name]=edited_email
+    elif selection=="4":
+        print("NEW CONTACT")
+        new_name=input("What is the name of your new contact?: ")
