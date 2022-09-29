d4e926f19269bb9c935321cd2c5a30ff847edf90
KuttaBanda <dreamzempire7@gmail.com>
2022-09-29 10:42:24 -0600

    Update student grade assignment.py

--- Second Language Assignments/student grade assignment.py
+++ Second Language Assignments/student grade assignment.py
@@ -31,14 +31,15 @@ while loop:
             if elem >= 80:
              print(elem,"%")
     elif selection == "3":
-        print("STATS")
+        print("STATS: ")
         print("Highest Grade: " , max_value, "%")
         print("Lowest Grade: " , min_value, "%")
         print("Average Grade: " , avg_value, "%")
     elif selection == "4":
-        newrandnums = []
         for i in range(35):
-                       
+            newrandnum=random.randint(0,100)
+            randnums[i]=newrandnum   
+        print("ALL GRADES HAVE BEEN RANDOMIZED")        
     elif selection == "5":
         print("EXITED")
         loop = False
