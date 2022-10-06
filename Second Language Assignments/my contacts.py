b8f8d0d262365a342722ad2f38f5afca787f0656
KuttaBanda <dreamzempire7@gmail.com>
2022-10-06 12:59:57 -0600

    Update my contacts.py

--- Second Language Assignments/my contacts.py
+++ Second Language Assignments/my contacts.py
@@ -25,32 +25,35 @@ while loop:
         print("DISPLAY CONTACT NAMES")
         for elem in names:
             print(elem)
+
     elif selection=="2":
         print("SEARCH FOR CONTACT")
         name_search=input("Who's contact name do you want to search?: ")
-        l1=[name_search]
-        for i in l1:
-            if i in names:
+        for i in contacts[0]:
+            if i==name_search:
                 index_name=names.index(name_search)
                 print("")
                 print(name_search)
                 print(contacts[1] [index_name])
                 print(contacts[2] [index_name])
-            else:
-                print("Name doesn't exist in contact list.")
+                break
+        else:
+            print("Name doesn't exist in contact list.")
+
     elif selection=="3":
         print("EDIT CONTACT")
         edit_name=input("Who's contact info do you want to change?: ")
-        l2=[edit_name]
-        for i in l2:
-            if i in names:
+        for name in contacts[0]:
+            if name==edit_name:
                 indexof_name=names.index(edit_name)
                 edited_num=input("What is the new number?: ")
                 contacts[1][indexof_name]=edited_num
                 edited_email=input("What is the new email?: ")
                 contacts[2] [indexof_name]=edited_email
-            else:
-                print("Name doesn't exist in contact list.")
+                break
+        else:
+            print("Name doesn't exist in contact list.")
+
     elif selection=="4":
         print("NEW CONTACT")
         new_name=str(input("What is the name of your new contact?: "))
@@ -59,18 +62,20 @@ while loop:
         contacts[1].append(new_number)
         new_email=str(input("What is the email for your new contact?: "))
         contacts[2].append(new_email)
+
     elif selection=="5":
         print("REMOVE CONTACT")
         remove_name=input("Who do you want to remove from contact list?: ")
-        l3=[remove_name]
-        for i in l3:
-            if i in names:
+        for i in contacts[0]:
+            if i==remove_name:
                 remove_index=names.index(remove_name)
                 contacts[0].pop(remove_index)
                 contacts[1].pop(remove_index)
                 contacts[2].pop(remove_index)
-            else:
-                print("Name doesn't exist in contact list.")
+                break
+        else:
+            print("Name doesn't exist in contact list.")
+
     elif selection=="6":
          print("EXITED")
          loop = False
