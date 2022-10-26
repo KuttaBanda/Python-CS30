49b69e9d9f358d5343925c1a110d1b4e62453396
KuttaBanda <dreamzempire7@gmail.com>
2022-10-26 13:27:38 -0600

    Create arrays.py

---
+++ array practice/arrays.py
@@ -0,0 +1,30 @@
+months=["January", "June", "July"]
+
+joy=[]
+for i in range(700):
+    joy.append("joy")
+
+number7=[]
+for i  in range(500):
+    number7.append("7")
+
+import random
+randomfloat=[]
+for i in range(5000):
+    randdec=round(random.uniform(0.0,99.9),1)
+    randomfloat.append(randdec)
+
+randomdecimal=[]
+for i in range(300):
+    randdec=round(random.uniform(0.0,39.9),1)
+    randomdecimal.append(randdec)
+
+multiples4=[range]
+for i in range(20, 801, 4):
+    multiples4.append(i)
+
+evens=[]
+for i in range(100, 9, -2):
+    evens.append(i)
+
+print(evens)
