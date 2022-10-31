73f5f8d918798d1306849f12bb19dfc3f83e705f
KuttaBanda <dreamzempire7@gmail.com>
2022-10-31 13:22:22 -0600

    Update data manage.py

--- data management/data manage.py
+++ data management/data manage.py
@@ -25,6 +25,7 @@ while loop:
     print("")
     print("Main Menu")
     print("1. Display all Data")
+    print("2. Display data based on criteria")
     selection=input("What option?: ")
 
     if selection=="1":
@@ -33,3 +34,7 @@ while loop:
             print(songs[i]["artist"])
             print(songs[i]["genre"])
             print("")
+    elif selection=="2":
+        properties=input("Which property do you want displayed?: ")
+        for i in range(len(songs)):
+            print(songs[i][properties])
