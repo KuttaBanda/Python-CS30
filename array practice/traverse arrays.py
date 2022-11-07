060fcee177ebfc3a9f6d73cf4859f8895d480399
KuttaBanda <dreamzempire7@gmail.com>
2022-11-07 13:12:32 -0700

    Update traverse arrays.py

--- array practice/traverse arrays.py
+++ array practice/traverse arrays.py
@@ -13,14 +13,42 @@
 
 # print(countY, countN, CountMay)
 
-ages=[1, 2, 3, 16, 18, 55, 96, 60, 10, 35]
+# ages=[1, 2, 3, 16, 18, 55, 96, 60, 10, 35]
 
-countUnder=0
-countOver=0
-for i in range(len(ages)):
-    if ages[i]< 18:
-        countUnder += 1
-    else:
-        countOver += 1
+# countUnder=0
+# countOver=0
+# for i in range(len(ages)):
+#     if ages[i]< 18:
+#         countUnder += 1
+#     else:
+#         countOver += 1
 
-print(countOver, countUnder)
+
+prices=[1, 2, 3, 4, 17, 19, 20, 21, 25, 48, 51, 67, 600]
+
+
+# # a. Count
+# countU20=0
+# countO20=0
+# countO50=0
+# for i in range(len(prices)):
+#     if prices[i]<20:
+#         countU20+=1
+#     elif prices[i]>=20 and prices[i]<=49:
+#         countO20+=1
+#     else:
+#         countO50+=1
+
+# # b. Increase by $2
+
+# for i in range(len(prices)):
+#     prices[i]=prices[i]+2
+
+# # c. Increase by 5%
+# for i in range(len(prices)):
+#     prices[i]=round(prices[i]*1.05,2)
+    
+# # d. Increase by 20%
+# for i in range(len(prices)):
+#     prices[i]=round(prices[i]*0.80,2)
+
