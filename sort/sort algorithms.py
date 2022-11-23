83b753b1dc0a4dbbb512d82848e93adb4d3937be
KuttaBanda <dreamzempire7@gmail.com>
2022-11-23 13:21:36 -0700

    Update sort algorithms.py

--- sort/sort algorithms.py
+++ sort/sort algorithms.py
@@ -1,20 +1,29 @@
-def bubbleSort(anArray):  
-    for i in range(0,len(anArray)-1):  
-        for j in range(len(anArray)-1):  
-            if(anArray[j]>anArray[j+1]):  
-                temp = anArray[j]  
-                anArray[j] = anArray[j+1]  
-                anArray[j+1] = temp  
-    return anArray  
+nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
+words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
+
+
+def bubbleSort(list1):  
+    for numcompare in range(len(list1)-1, 0, -1):  
+        for j in range(numcompare):  
+            if(list1[j]>list1[j+1]):  
+                temp = list1[j]  
+                list1[j] = list1[j+1]  
+                list1[j+1] = temp  
+    return list1  
+
+
 
 def selectionSort(anArray):
     for i in range(len(anArray)-1):
-        minIndex=i
+        minposition=i
         for j in range(i+1, len(anArray)):
-            if anArray[j]<anArray[minIndex]:
-                minIndex=j
-        anArray[i], anArray[minIndex]=anArray[minIndex], anArray[i]
+            if anArray[j]<anArray[minposition]:
+                minposition=j
+
+        anArray[minposition], anArray[i]=anArray[i], anArray[minposition]
     return anArray
+
+
 
 def insertionSort(anArray):
     for i in range(1, len(anArray)):
@@ -27,4 +36,5 @@ def insertionSort(anArray):
     return anArray
 
 
-
+print(insertionSort(nums))
+print(insertionSort(words))
