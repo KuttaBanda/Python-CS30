86b0a2817c674254e38d417c33eda44f70f91579
KuttaBanda <dreamzempire7@gmail.com>
2023-01-13 08:48:05 -0700

    Update RPS.py

--- portfolio/RPS.py
+++ portfolio/RPS.py
@@ -1,24 +1,4 @@
-from random import randint
 
-
-choices=["rock", "paper","scissors"]
-
-class player:
-    def __init__(self):
-        self.choice="None"
-        self.score= 0
-
-    def getInput(self, choice, run):
-        for item in choices:
-            if item==choice:
-                print('Item selected')
-                self.choice=choice
-                return run
-        print("Game Ended")
-        run=False
-        return run    
-
-        
 
 
 
